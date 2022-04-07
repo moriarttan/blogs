f1ac1e4110e8de6b23c728e364cfb4ce3f58ecc5
谭慧东 <15755566506@163.com>
2022-04-07 19:35:44 +0800

    新增MyBatis test案例

---
+++ src/main/java/com/example/blogs/mapper/TestMapper.java
@@ -0,0 +1,11 @@
+package com.example.blogs.mapper;
+
+import com.example.blogs.test.domain.Test;
+
+import java.util.List;
+
+/* 持久层 */
+public interface TestMapper {
+
+    public List<Test> list();
+}
--- src/main/java/com/example/blogs/test/controller/TestController.java
+++ src/main/java/com/example/blogs/test/controller/TestController.java
@@ -1,13 +1,20 @@
 package com.example.blogs.test.controller;
 
+import com.example.blogs.service.TestService;
+import com.example.blogs.test.domain.Test;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import javax.xml.ws.RequestWrapper;
+import java.util.List;
 
 @RestController
 @RequestMapping("/test")
 public class TestController {
+
+    @Resource
+    private TestService testService;
     /**
      * 请求方式
      * GET, POST, PUT, DELETE
@@ -16,5 +23,10 @@ public class TestController {
     @RequestMapping("/hello")
     public String hello () {
         return "Hello World!";
+    }
+
+    @RequestMapping("/list")
+    public List<Test> list () {
+        return testService.list();
     }
 }
